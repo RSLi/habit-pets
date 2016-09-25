a2229beb8d548c0711beeb6f17098c0607a2f0bb
RSLi <robertsli@163.com>
2016-09-25 07:04:56 -0400

    bug not solved

--- js/udata_json_util.js
+++ js/udata_json_util.js
@@ -33,6 +33,8 @@ function udata_post() {
     hiddenField.setAttribute("value", JSON.stringify(udata));
     form.appendChild(hiddenField);
     document.body.appendChild(form);
+    alert(JSON.stringify(udata));
+
     form.submit();
 }
 
